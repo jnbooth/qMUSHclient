50cc56cfeedcf643644005f3ecc398e366fc66a8
Joshua Booth <joshua.n.booth@gmail.com>
2024-01-07 14:43:32 -0800

    fix exports

--- qt/qt/src/widgets/mod.rs
+++ qt/qt/src/widgets/mod.rs
@@ -1,21 +1,21 @@
 mod abstract_scroll_area;
 use abstract_scroll_area::AbstractScrollAreaBinding;
-pub use abstract_scroll_area::QAbstractScrollArea;
+pub use abstract_scroll_area::RAbstractScrollArea;
 
 mod dialog;
-pub use dialog::QDialog;
+pub use dialog::RDialog;
 
 mod frame;
-pub use frame::QFrame;
+pub use frame::RFrame;
 
 mod line_edit;
-pub use line_edit::QLineEdit;
+pub use line_edit::RLineEdit;
 
 mod message_box;
-pub use message_box::QMessageBox;
+pub use message_box::RMessageBox;
 
 mod text_browser;
-pub use text_browser::QTextBrowser;
+pub use text_browser::RTextBrowser;
 
 mod text_edit;
 use text_edit::TextEditBinding;
