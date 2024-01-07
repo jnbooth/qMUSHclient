63316b05ba06dba4e8443d242030ea4d4a7972a6
Joshua Booth <joshua.n.booth@gmail.com>
2024-01-07 14:54:48 -0800

    use QCoreApplication

--- qmushclient/src/bin/main.rs
+++ qmushclient/src/bin/main.rs
@@ -1,27 +1,23 @@
 #![windows_subsystem = "windows"]
 
 use qmushclient::{App, Widget};
-use qt::{QLocale, QTranslator};
-use qt_core::{ApplicationAttribute, QCoreApplication, QString};
+use qt::{QCoreApplication, QLocale, QTranslator};
+use qt_core::ApplicationAttribute;
 use qt_widgets::QApplication;
 
 fn load_translator() {
-    unsafe {
-        let locale = QLocale::new();
-        let translator = QTranslator::new();
-        let translate = translator.load(&locale, "app", "_", ":/translations", ".qm");
-        if translate {
-            QCoreApplication::install_translator(translator.get_translator());
-        }
+    let locale = QLocale::new();
+    let translator = QTranslator::new();
+    let translate = translator.load(&locale, "app", "_", ":/translations", ".qm");
+    if translate {
+        QCoreApplication::install_translator(&translator);
     }
 }
 
 fn main() {
-    unsafe {
-        QCoreApplication::set_attribute_2a(ApplicationAttribute::AAShareOpenGLContexts, true);
-        QCoreApplication::set_organization_name(&QString::from_std_str("qMUSHclient"));
-        QCoreApplication::set_organization_domain(&QString::from_std_str("qMUSHclient"));
-    }
+    QCoreApplication::set_attribute(ApplicationAttribute::AAShareOpenGLContexts, true);
+    QCoreApplication::set_organization_name("qMUSHclient");
+    QCoreApplication::set_organization_domain("qMUSHclient");
     QApplication::init(|_| {
         load_translator();
 
--- qt/qt/src/locale.rs
+++ qt/qt/src/locale.rs
@@ -36,12 +36,6 @@ impl From<QBox<q::QTranslator>> for QTranslator {
 }
 
 impl QTranslator {
-    pub fn get_translator(&self) -> &QBox<q::QTranslator> {
-        &self.inner
-    }
-}
-
-impl QTranslator {
     pub fn new() -> Self {
         Self {
             inner: unsafe { q::QTranslator::new_0a() },
